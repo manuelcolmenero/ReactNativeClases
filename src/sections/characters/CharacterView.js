14c82536b1f743e42923f03e56a83a8d9846a935
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-12-20 20:30:57 +0100

    Ajuste de la función a la que se llama en el botón de detalle de personaje para poder eliminar registros

--- src/sections/characters/CharacterView.js
+++ src/sections/characters/CharacterView.js
@@ -1,44 +1,36 @@
 // Imports
 import React, { Component } from 'react'
 import { View, StyleSheet, Image, Text, Dimensions } from 'react-native'
-import { connect } from  'react-redux'
+import { connect } from 'react-redux'
 
 import { Button } from 'react_native_app/src/widgets'
 import { Colors } from 'react_native_app/src/commons'
 import * as CharactersActions from 'react_native_app/src/redux/actions/characters'
 
 class CharacterView extends Component {
-    // Propiedades por defecto para prevenir errores y documentar el componente
-    static defaultProps = {
-        character: {},
-    }
 
-    onSubmit() {
 
-    }
-
-    onDelete(character){
+    onDelete(character) {
         this.props.deleteCharacter(character)
-
     }
 
     render() {
         const { character } = this.props
         const nombre = character ? character.nombre : ''
-        const edad   = character ? character.edad : ''
-        const image  = character && character.image_dir ? { uri: character.image_dir } : require('react_native_app/src/resources/placeholder.png')
+        const edad = character ? character.edad : ''
+        const image = character && character.image_dir ? { uri: character.image_dir } : require('react_native_app/src/resources/placeholder.png')
 
         return (
             <View style={styles.container}>
 
-                <Image source={image} style={styles.image} resizeMode={'cover'} /> 
+                <Image source={image} style={styles.image} resizeMode={'cover'} />
                 <View style={styles.textContainer}>
-                    <Text style={styles.name}>{ nombre }</Text>
-                    <Text style={styles.edad}>{ 'Edad: ' + edad }</Text>
+                    <Text style={styles.name}>{nombre}</Text>
+                    <Text style={styles.edad}>{'Edad: ' + edad}</Text>
                 </View>
 
                 <View style={styles.buttonContainer}>
-                    <Button label={'Eliminar'} onPress={ () => this.onSubmit(character) } isFetching={this.props.isFetching} />
+                    <Button label={'Eliminar'} onPress={() => this.onDelete(character)} isFetching={this.props.isFetching} />
                 </View>
 
             </View>
@@ -54,19 +46,22 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch, props) => {
     return {
-        // Se descarga el listado de personajes asociados a una casa
         deleteCharacter: (character) => {
-
-            // Si existe character realiza el dispatch
+            /*
+            if(character) {
+                dispatch(CharactersActions.deleteCharacter(character))
+            }
+            */
             character && dispatch(CharactersActions.deleteCharacter(character))
         },
     }
 }
 
+
 export default connect(mapStateToProps, mapDispatchToProps)(CharacterView)
 
 const styles = StyleSheet.create({
-    
+
     container: {
         flex: 1,
         backgroundColor: Colors.containerBackground,
